960dfc94a70bf72fb3307f738bb6ef1c65b7fc04
MMPAC <melvin.portillo@unitec.edu>
2016-09-16 15:25:39 -0600

    Analisis Sintactico

---
+++ AnalisisSintactico.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lenguajes;
+
+/**
+ *
+ * @author alvarez
+ */
+public class AnalisisSintactico {
+    public static void main(String[] args) {
+        //No se puede poner un numero para declarar una variable
+        int[] 5numeros = new int[4];
+        //Aqui podemos observar dos errores uno es la x no declarada
+        x=numeros[3];
+        //falto el punto y coma
+        int b
+    }
+    
+}
